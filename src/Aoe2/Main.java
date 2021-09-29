e81e01e487609a7abd843449c3582fc666b7e509
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-09-29 17:06:09 +0200

    jdu blít

--- src/Aoe2/Main.java
+++ src/Aoe2/Main.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) throws IOException {
         File file = new File("src/Aoe2/input.txt");
-        ArrayList<String> input = new ArrayList<String>();
+        ArrayList<String> input = new ArrayList<>();
 
         BufferedReader br = new BufferedReader(new FileReader(file));
 
@@ -14,6 +14,24 @@ public class Main {
         while ((st = br.readLine()) != null)
             input.add(st);
 
+        System.out.println("Part one: " + partOne(input));
+        System.out.println("Part two: " + outPTwo(input));
+    }
+
+    static int outPTwo(ArrayList<String> input) {
+        int out = 0;
+        for (String s : input) {
+            String[] prismVals = s.split("x");
+
+            int[] values = {Integer.parseInt(prismVals[0]), Integer.parseInt(prismVals[1]), Integer.parseInt(prismVals[2])};
+            int[] sortedVals = sortNumbers(values);
+
+            out += (sortedVals[0] * sortedVals[1] * sortedVals[2]) + 2 * (sortedVals[0] + sortedVals[1]);
+        }
+        return out;
+    }
+
+    static int partOne(ArrayList<String> input) {
         int out = 0;
         for (String s : input) {
             String[] prismVals = s.split("x");
@@ -21,20 +39,24 @@ public class Main {
             int l = Integer.parseInt(prismVals[0]) * Integer.parseInt(prismVals[1]);
             int w = Integer.parseInt(prismVals[1]) * Integer.parseInt(prismVals[2]);
             int h = Integer.parseInt(prismVals[0]) * Integer.parseInt(prismVals[2]);
-            int[] values = {l,w,h};
+            int[] values = {l, w, h};
+            int[] sortedVals = sortNumbers(values);
 
-            out += 2*(l+w+h) + lowestNumber(values);
-        }
-        System.out.println(out);
-    }
-
-    static int lowestNumber(int[] numbers){
-        int out = numbers[0];
-        for (int number : numbers) {
-            if (number < out) {
-                out = number;
-            }
+            out += 2 * (l + w + h) + sortedVals[0];
         }
         return out;
+    }
+
+    static int[] sortNumbers(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = 0; j < numbers.length; j++) {
+                if (numbers[i] <= numbers[j]) {
+                    int holder = numbers[j];
+                    numbers[j] = numbers[i];
+                    numbers[i] = holder;
+                }
+            }
+        }
+        return numbers;
     }
 }
