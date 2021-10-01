52bb4403a738e93a593853ac4617f9051a643ba4
MrHatkeeper <karel.krch@educanet.cz>
2021-10-01 09:47:42 +0200

    SUma

---
+++ src/Suma.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Suma {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Integer> input = new ArrayList<>();
+        do {
+            input.add(sc.nextInt());
+
+        } while (!sc.nextLine().equals("end"));
+        int out = 0;
+        for (Integer integer : input) {
+            out += integer;
+        }
+        System.out.println(out);
+    }
+}
